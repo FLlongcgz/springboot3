48e1857dfb4ae1b4d961a1b0b97624b720d43c3f
xiaolou <longfl_456@163.com>
2019-11-18 18:53:04 +0800

    上传到github

---
+++ src/main/java/com/xiaolou/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.xiaolou.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 小楼
+ * Date: 2019/11/18
+ * Time: 18:47
+ * Description:
+ */
+
+public class HelloController {
+
+    public void hello(){
+
+        System.out.println("Hello");
+    }
+}
